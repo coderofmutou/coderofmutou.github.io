b923ac090db2522d1727982ee99a915a3e007a06
coderofmutou <1214025526@qq.com>
2024-05-01 17:21:15 +0800

    修改Github Actions等配置文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -224,7 +224,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2024, // 博客创建年份
       copyrightInfo:
-          'bombax | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+          'bombax | <a href="https://github.com/coderofmutou/coderofmutou.github.io/blob/main/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
