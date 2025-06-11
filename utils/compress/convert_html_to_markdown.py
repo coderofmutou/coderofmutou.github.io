3983c942de7166c5063a2a6c8109cbf6f00fe9a5
bombax <1214025526@qq.com>
2025-06-12 00:25:27 +0800

    移除文本规则zh-half-and-full-width-bracket，其会导致全角/半角括号不对应，修复笔记中部分括号中英文不对应错误

--- utils/compress/convert_html_to_markdown.py
+++ utils/compress/convert_html_to_markdown.py
@@ -12,17 +12,20 @@ import os
 def convert_html_to_markdown(html_text):
     # 改进的正则表达式匹配 <img> 标签，提取 src 和 alt 属性
     # alt 属性是可选的，如果没有 alt，则默认使用文件名
-    img_tag_pattern = r'<img\s+src="([^"]+)"\s+alt="([^"]*)"?[^>]*>'
+    img_tag_pattern = re.compile(r'<img\s+[^>]*?src=["\']([^"\']+)["\'][^>]*(?:\s+alt=["\']([^"\']*)["\'])?[^>]*>', re.IGNORECASE)
+
+    def process_match(match):
+        src = match.group(1)
+        alt = match.group(2) if match.group(2) else os.path.basename(src)
+        # 统一路径分隔符为/
+        normalized_src = src.replace('\\', '/')
+        return f'![{alt}]({normalized_src})'
 
     # 检查是否有 <img> 标签需要转换
     if re.search(img_tag_pattern, html_text):
         # 使用正则表达式将 HTML <img> 标签转换为 Markdown 格式
         # 如果 alt 为空，提取文件名作为 alt 值
-        html_text = re.sub(
-            img_tag_pattern,
-            lambda m: f'![{m.group(2) if m.group(2) else os.path.basename(m.group(1))}]({m.group(1)})',
-            html_text
-        )
+        html_text = re.sub(img_tag_pattern, process_match, html_text)
         return html_text, True  # 返回转换后的文本和是否有替换
     else:
         return html_text, False  # 如果没有替换，直接返回原文本
@@ -49,8 +52,8 @@ def process_html_file(input_file, output_file):
 
 if __name__ == "__main__":
     # 示例文件路径（可以修改为你自己的文件路径）
-    directory = r'\docs\01.Java基础\Java从入门到精通(JDK17版)\尚硅谷_第02章_变量与运算符'
-    input_markdown_file = os.path.join(directory, '尚硅谷_宋红康_第02章_变量与运算符.md')   # 输入 Markdown 文件路径
+    directory = r'docs\01.Java基础\20.核心\21.MySQL从入门到高级-高级篇'
+    input_markdown_file = os.path.join(directory, '第19章_数据库备份与恢复.md')   # 输入 Markdown 文件路径)
     output_markdown_file = input_markdown_file.replace('.md', '_new.md')   # 输出的 Markdown 文件路径
 
     # 执行文件转换，并获取最终的 output_markdown_file
